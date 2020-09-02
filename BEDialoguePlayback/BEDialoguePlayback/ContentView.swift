3bda9077cb3ad91ee152462d5c91918dd62ae41e
Oleg Fedyakin <acid.lynx@ya.ru>
2020-09-02 05:29:28 +0300

    feat: add json and show it

--- BEDialoguePlayback/BEDialoguePlayback/ContentView.swift
+++ BEDialoguePlayback/BEDialoguePlayback/ContentView.swift
@@ -9,13 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var dialogueLinesModel: [DialogueLine]
+        
     var body: some View {
-        Text("Hello, World!")
+        return List {
+            ForEach(dialogueLinesModel, id: \.self) { item in
+                VStack {
+                    Text(item.line)
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let dialogueLines = getDialogLines()
+        return ContentView(dialogueLinesModel: dialogueLines)
     }
 }
