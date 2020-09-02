349c5a9e40eedf13dfe1848f60b48acfa43b540c
Oleg Fedyakin <acid.lynx@ya.ru>
2020-09-02 04:56:08 +0300

    chore: add initial project

---
+++ BEDialoguePlayback/BEDialoguePlayback/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BEDialoguePlayback
+//
+//  Created by User on 02.09.2020.
+//  Copyright © 2020 Binira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
